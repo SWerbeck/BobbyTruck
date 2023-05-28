1e9d2117f8cff184d62b6ea88596b35f36b00a06
Stephen Werbeck <SWerbeck@yahoo.com>
2023-05-28 19:59:09 -0400

    started project

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import ApplyComponent from './ApplyComponent/ApplyComponent'
+import './App.css'
+
+function App() {
+ 
+
+  return (
+    <>
+   <ApplyComponent />
+    </>
+  )
+}
+
+export default App
---
+++ src/ApplyComponent/ApplyComponent.jsx
@@ -0,0 +1,28 @@
+
+
+
+const ApplyComponent = () => {
+
+return (
+  <div>
+   <h1>Apply For Position</h1>
+    <form>
+        <label>name</label>
+        <input type='text' name='name'/>
+
+        <label>email</label>
+        <input type='email' email='user_email'/>
+
+        <label>Message</label>
+        <textarea name='message' rows='4'/>
+
+        <input type='submit' value='Send'/>
+    </form>
+  </div>
+
+)
+
+}
+
+
+export default ApplyComponent
